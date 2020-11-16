bfbbc7cde16bce6d521aeb6e81aecc32e3f26923
KirkHietpas <hietpaskirk@gmail.com>
2020-11-15 17:45:44 -0700

    now have two shuffle functions

--- src/assignment04 /assignment04/TimingMergeSort.java
+++ src/assignment04 /assignment04/TimingMergeSort.java
@@ -2,7 +2,6 @@ package assignment04;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Random;
 
 public class TimingMergeSort {
@@ -14,13 +13,22 @@ public class TimingMergeSort {
         int N = (int) Math.pow(2, pow); //this will be the size of the list every time
 
 
+        ArrayList<Integer> randList1 = TimingMergeSort.shuffleListNonRandom(20);
+        ArrayList<Integer> randList2 = TimingMergeSort.shuffleListNonRandom(20);
+        ArrayList<Integer> randList3 = TimingMergeSort.shuffleListNonRandom(20);
+        ArrayList<Integer> randList4 = TimingMergeSort.shuffleListNonRandom(20);
 
+        System.out.println(randList1);
+        System.out.println(randList2);
+        System.out.println(randList3);
+        System.out.println(randList4);
 
 
     }
 
     /**
      * generates an array list of specified size in sorted order
+     *
      * @param Size
      * @return
      */
@@ -35,6 +43,7 @@ public class TimingMergeSort {
 
     /**
      * Genearates an randomly Orders list of specified size
+     *
      * @param size
      * @return
      */
@@ -50,6 +59,7 @@ public class TimingMergeSort {
 
     /**
      * generates a list that is sorted in reverse order
+     *
      * @param size
      * @return
      */
@@ -61,13 +71,40 @@ public class TimingMergeSort {
         }
         return worstCaseArrayList;
     }
-    //shuffle function
-    public static void shuffleList(ArrayList arrayList) {
+
+    /**
+     * returns a shuffled array always in the same order
+     *
+     * @param size
+     * @return
+     */
+    public static ArrayList<Integer> shuffleListNonRandom(int size) {
+        ArrayList<Integer> randomList = new ArrayList<>();
+        for (int i = 0; i < size; i++) { //populate a sorted list
+            randomList.add(i);
+        }
+
+        Random ran = new Random(size);
+        for (int i = 0; i < size; i++) {
+            int nxt = ran.nextInt(size);
+            Collections.swap(randomList, i, nxt);
+        }
+        return randomList;
+    }
+
+    /**
+     * This randomly shuffles a list
+     *
+     * @param arrayList
+     */
+    public static void shuffleListRandom(ArrayList arrayList) {
         Random ran = new Random();
         for (int i = 0; i < arrayList.size(); i++) {
             int nxt = ran.nextInt(arrayList.size());
             Collections.swap(arrayList, i, nxt);
         }
+
+
     }
 
 }
